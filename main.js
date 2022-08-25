95316f12a15344d1f8fa62fa7c2ea2f771e64a47
momomohi <mohammed.mudassir.mohiuddin@oracle.com>
2022-08-25 17:44:17 +0530

    fix: hamburger close icon update on click

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const primaryHeader = document.querySelector('.primary-header');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const primaryNav = document.querySelector('.primary-navigation');
-
+const iconHamburger = document.querySelector('.icon-hamburger');
+const iconClose = document.querySelector('.icon-close');
 
 navToggle.addEventListener('click', () => {
-  primaryHeader.hasAttribute('data-visible')
-    ? navToggle.setAttribute('aria-expanded', false)
-    : navToggle.setAttribute('aria-expanded', true);
+  if (primaryHeader.hasAttribute('data-overlay')){
+      navToggle.setAttribute('aria-expanded', false);
+      iconHamburger.style.display  = 'block';
+      iconClose.style.display  = 'none';
+  }
+  else {
+      navToggle.setAttribute('aria-expanded', true)
+      iconHamburger.style.display  = 'none';
+      iconClose.style.display  = 'block';
+    };
   primaryNav.toggleAttribute('data-visible');
   primaryHeader.toggleAttribute('data-overlay');  
+  //iconHamburger.toggleAttribute('data-visible');
+  //iconClose.toggleAttribute('data-visible');
 });
